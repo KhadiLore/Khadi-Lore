d60c745f931ac35243a9cc669ef1c1c921842840
Khadi Lore <hridayanphukan09@gmail.com>
2025-03-01 16:28:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const allImages = document.querySelectorAll(".images .img");
+const lightbox = document.querySelector(".lightbox");
+const closeImgBtn = lightbox.querySelector(".close-icon");
+
+allImages.forEach(img => {
+    // Calling showLightBox function with passing clicked img src as argument
+    img.addEventListener("click", () => showLightbox(img.querySelector("img").src));
+});
+
+const showLightbox = (img) => {
+    // Showing lightbox and updating img source
+    lightbox.querySelector("img").src = img;
+    lightbox.classList.add("show");
+    document.body.style.overflow = "hidden";
+}
+
+closeImgBtn.addEventListener("click", () => {
+    // Hiding lightbox on close icon click
+    lightbox.classList.remove("show");
+    document.body.style.overflow = "auto";
+});
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
